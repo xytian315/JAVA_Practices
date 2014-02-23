4b57453df05b69e1869acb88b7c9dc57a79e1b38
Xiaoyi Tian <xytian315@gmail.com>
2014-02-22 19:59:11 -0800

    Create isPalindrome.java

---
+++ isPalindrome.java
@@ -0,0 +1,14 @@
+/*
+Write code to check a String is palindrome or not?
+
+*/
+public class Solution{
+	public static boolean isPalindrome(String str){
+		for (int i=0; i < str.length()/2; i++){
+			if(str.charAt(i)!= str.charAt(str.length-1-i))
+				return false;
+		}
+		return true;
+	}
+
+}
