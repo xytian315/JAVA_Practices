75c26e6ad40a6eac9f25081389ba5bdd44628603
Xiaoyi Tian <xytian315@gmail.com>
2014-03-31 17:08:38 -0700

    Update 32StackMin.java

--- 32StackMin.java
+++ 32StackMin.java
@@ -2,6 +2,7 @@
 3.2 Design a stack which, in addition to push and pop, also has a function 
 min which returns the minimum element?push, pop, and min should all operate in O(1) time
 
+implement min heap 
 
 hint: use another stack to track min value
 
