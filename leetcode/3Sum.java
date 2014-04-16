db7bc447e995075e8562fa6db47fbbc2ae069b17
Xiaoyi Tian <xytian315@gmail.com>
2014-04-15 23:57:56 -0700

    Create 3Sum.java

---
+++ leetcode/3Sum.java
@@ -0,0 +1,62 @@
+/*
+leetcode: 3Sum 
+
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+The solution set must not contain duplicate triplets.
+    For example, given array S = {-1 0 1 2 -1 -4},
+
+    A solution set is:
+    (-1, 0, 1)
+    (-1, -1, 2)
+
+*/
+
+
+import java.util.Hashtable;
+public class Solution {
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        //the result: 
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        
+        Arrays.sort(num);
+        
+        for(int i=0; i< num.length-2; i++){
+            int negative = - num[i];
+            
+            int start = i+1;
+            int end = num.length -1;
+            
+            while(start<end){
+                if(num[start]+num[end]== negative){
+                    ArrayList<Integer> arr = new ArrayList<Integer>();
+                    arr.add(i);
+                    arr.add(start);
+                    arr.add(end);
+                    result.add(arr);
+                }else if(num[start]+num[end] < negative){
+                    start++;
+                }else
+                    end--;
+                
+            }
+        }
+        return result;
+        
+        
+    }
+}
+
+
+/*
+note : 
+
+先排序,然后左右夹逼,复杂度 O(n2)。
+这个方法可以推广到 k-sum,先排序,然后做 k − 2 次循环,在最内层循环左右夹逼,时间复杂 度是 O(max{n log n, nk−1 })。
+
+
+don't forget to check the duplicates!!!!!
+
+*/
