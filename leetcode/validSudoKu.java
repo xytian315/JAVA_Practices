461f2aee3188078ecec3c934f669ee0d6dff899d
Xiaoyi Tian <xytian315@gmail.com>
2014-04-17 10:52:52 -0700

    Update and rename validSudocu.java to validSudoKu.java

--- leetcode/validSudoKu.java
+++ leetcode/validSudoKu.java
@@ -17,57 +17,56 @@ A valid Sudoku board (partially filled) is not necessarily solvable. Only the fi
 
 
 public class Solution{
-
-	public boolean isValidSudoku(char[][] board){
-			
-		boolean[] checker = new boolean[9];
-		
-		//check each row
-		for(int i =0; i<board.length; i++){
-			for(int j=0; j< board[0].length;j++){
+    public boolean isValidSudoku(char[][] board){
+	    //check each row
+		for(int i =0; i<9; i++){
+			boolean[] checker = new boolean[10];
+			for(int j=0; j< 9;j++){
 				if(board[i][j]=='.')
 					continue;
-					
 				int temp = board[i][j]-'0';
 				if(checker[temp])
 					return false;
 				checker[temp]= true;
 			}
 		}	
-		for(int i =0; i<=9;i++)
-			checker[i]=false;
-		//check each col
-		for(int i =0; i< board[0].length;i++){
-			for(int j =0; j< board.length;j++){
+	    //check each col
+		for(int i =0; i< 9;i++){
+			boolean[] checker = new boolean[10];
+			for(int j =0; j< 9;j++){
 				if(board[j][i] == '.')
 					continue;
 				int temp = board[j][i] -'0';
 				if(checker[temp])
 					return false;
 				checker[temp]=true;
+			}
 		}
-		
+
 		//check each subbox
-		for(int i =0; i<=9;i++)
-			checker[i]=false;
-			
 		for(int r =0; r<3; r++){
 			for(int c=0; c<3; c++){
+				boolean[] checker = new boolean[10];
 				for(int i =3*r; i< 3*r+3; i++){
 					for(int j = 3*c; j<3*c+3; j++){
-						board[i][j]=
-						
-					}	
+						if(board[i][j] == '.')
+							continue;
+						int temp = board[i][j]-'0';
+						if(checker[temp])
+							return false;
+						checker[temp] = true;
+					}
 				}
 			}
 		}
-		
-	
+	    return true;
 	}
 }
 
 /*
 should we just check the case 9 row, 9 col, 9 subbox???
 the first mistake: boolean checker index from 0 not 1
-have difficulty to loop subbox
+have difficulty to loop subbox , 
+
+and note the matched {} of "for" loop
 */
